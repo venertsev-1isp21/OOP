8481c8d46a4ef4906b524551c6888222ff18d130
Venertsev-1ISP21 <senavener@gmail.com>
2025-03-13 23:06:41 +0700

    four commin

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from PyQt6.QtWidgets import QApplication, QPushButton, QLabel, QVBoxLayout, QWid
 
 
 class Person(ABC):
+    def __init__(self, name):
+        self._name = name
+
     @abstractmethod
     def get_name(self):
         pass
@@ -15,7 +18,7 @@ class User(Person):
     users_count = 0
 
     def __init__(self, name_user, weight, height, years_old, gender):
-        self.__name_user = name_user
+        super().__init__(name_user)
         self.__weight = int(weight)
         self.__height = int(height)
         self.__years_old = int(years_old)
@@ -23,10 +26,10 @@ class User(Person):
         User.users_count += 1
 
     def show_data(self):
-        return [self.__name_user, self.__weight, self.__height, self.__years_old]
+        return [self._name, self.__weight, self.__height, self.__years_old]
 
     def get_name(self):
-        return self.__name_user
+        return self._name
 
     def get_weight(self):
         return self.__weight
@@ -63,6 +66,9 @@ class Food:
     def __str__(self):
         return f"{self.__name_food}: {self.__calories} ккал, {self.__proteins} г белков, {self.__fats} г жиров, {self.__carbohydrates} г углеводов"
 
+    def __repr__(self):
+        return f"Food('{self.__name_food}', {self.__calories}, {self.__proteins}, {self.__fats}, {self.__carbohydrates})"
+
     def __add__(self, other):
         if isinstance(other, Food):
             return Food(
@@ -73,6 +79,9 @@ class Food:
                 self.__carbohydrates + other.__carbohydrates
             )
         raise TypeError("Можно складывать только объекты типа Food")
+
+    def get_calories(self):
+        return self.__calories
 
 
 class Meal:
@@ -88,6 +97,25 @@ class Meal:
     def __str__(self):
         return "\n".join(str(food) for food in self.food_items)
 
+
+class FoodManager:
+    @staticmethod
+    def find_max_calories(food_list):
+        if not food_list or not any(row for row in food_list):
+            return None
+
+        max_food = None
+        max_calories = -1
+
+        for row in food_list:
+            for food in row:
+                if food and food.get_calories() > max_calories:
+                    max_calories = food.get_calories()
+                    max_food = food
+
+        return max_food
+
+
 class BreakFast:
     def __init__(self, list_of_meal):
         self.__list_of_meal = list_of_meal
@@ -95,26 +123,31 @@ class BreakFast:
     def get_list(self):
         return self.__list_of_meal
 
-class Dinner:
+
+class Dinner(BreakFast):
     def __init__(self, list_of_meal):
-        self.__list_of_meal = list_of_meal
+        super().__init__(list_of_meal)
 
-    def get_list(self):
-        return self.__list_of_meal
+    def show_meals(self):
+        return self.get_list()
 
-class Lunch:
-    def __init__(self, list_of_meal):
-        self.__list_of_meal = list_of_meal
 
-    def get_list(self):
-        return self.__list_of_meal
+class Lunch(Dinner):
+    def __init__(self, list_of_meal, extra_meal):
+        super().__init__(list_of_meal)
+        self._extra_meal = extra_meal
 
-class Snack:
-    def __init__(self, list_of_meal):
-        self.__list_of_meal = list_of_meal
+    def get_extra_meal(self):
+        return self._extra_meal
 
-    def get_list(self):
-        return self.__list_of_meal
+
+class Snack(Lunch):
+    def __init__(self, list_of_meal, extra_meal):
+        super().__init__(list_of_meal, extra_meal)
+
+    def show_all_meals(self):
+        return self.get_list() + [self.get_extra_meal()]
+
 
 class ChildWindow(QDialog):
     def __init__(self, user):
@@ -193,26 +226,10 @@ class MainWindow(QWidget):
         self.button.setFixedSize(100, 30)
         self.button.clicked.connect(self.set_user_data)
 
-        self.layout.addWidget(self.user_label)
-        self.layout.setAlignment(self.user_label, Qt.AlignmentFlag.AlignCenter)
-
-        self.layout.addWidget(self.user_name)
-        self.layout.addWidget(self.enterName)
-
-        self.layout.addWidget(self.user_Weight)
-        self.layout.addWidget(self.enterWeight)
-
-        self.layout.addWidget(self.user_Height)
-        self.layout.addWidget(self.enterHeight)
-
-        self.layout.addWidget(self.user_Age)
-        self.layout.addWidget(self.enterAge)
-
-        self.layout.addWidget(self.user_Gender)
-        self.layout.addWidget(self.enterGender)
-
-        self.layout.addWidget(self.button)
-        self.layout.setAlignment(self.button, Qt.AlignmentFlag.AlignCenter)
+        for widget in [self.user_label, self.user_name, self.enterName, self.user_Weight, self.enterWeight,
+                       self.user_Height, self.enterHeight, self.user_Age, self.enterAge, self.user_Gender,
+                       self.enterGender, self.button]:
+            self.layout.addWidget(widget)
 
         self.setLayout(self.layout)
         self.show()
