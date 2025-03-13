236420a42731d4a04fcc2e6f5616f78d02d936db
Venertsev-1ISP21 <senavener@gmail.com>
2025-03-13 21:52:54 +0700

    third commit

--- main.py
+++ main.py
@@ -1,48 +1,152 @@
 import sys
-
+from abc import ABC, abstractmethod
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QApplication, QPushButton, QLabel, QVBoxLayout, QWidget, QLineEdit, QDialog, QTableWidget, QTableWidgetItem
+from PyQt6.QtWidgets import QApplication, QPushButton, QLabel, QVBoxLayout, QWidget, QLineEdit, QDialog, QTableWidget, \
+    QTableWidgetItem
+
+
+class Person(ABC):
+    @abstractmethod
+    def get_name(self):
+        pass
+
+
+class User(Person):
+    users_count = 0
+
+    def __init__(self, name_user, weight, height, years_old, gender):
+        self.__name_user = name_user
+        self.__weight = int(weight)
+        self.__height = int(height)
+        self.__years_old = int(years_old)
+        self.__gender = gender.lower()
+        User.users_count += 1
+
+    def show_data(self):
+        return [self.__name_user, self.__weight, self.__height, self.__years_old]
+
+    def get_name(self):
+        return self.__name_user
+
+    def get_weight(self):
+        return self.__weight
+
+    def get_height(self):
+        return self.__height
+
+    def get_age(self):
+        return self.__years_old
+
+    def get_gender(self):
+        return self.__gender
+
+    def calories_culc(self):
+        if self.__gender == 'male':
+            return (10 * self.__weight) + (6.25 * self.__height) - (5 * self.__years_old) + 5
+        elif self.__gender == 'female':
+            return (10 * self.__weight) + (6.25 * self.__height) - (5 * self.__years_old) - 161
+        return 0
+
+    @staticmethod
+    def get_users_count():
+        return User.users_count
+
+
+class Food:
+    def __init__(self, name_food, calories, proteins, fats, carbohydrates):
+        self.__name_food = name_food
+        self.__calories = int(calories)
+        self.__proteins = int(proteins)
+        self.__fats = int(fats)
+        self.__carbohydrates = int(carbohydrates)
+
+    def __str__(self):
+        return f"{self.__name_food}: {self.__calories} ккал, {self.__proteins} г белков, {self.__fats} г жиров, {self.__carbohydrates} г углеводов"
+
+    def __add__(self, other):
+        if isinstance(other, Food):
+            return Food(
+                "Смешанное блюдо",
+                self.__calories + other.__calories,
+                self.__proteins + other.__proteins,
+                self.__fats + other.__fats,
+                self.__carbohydrates + other.__carbohydrates
+            )
+        raise TypeError("Можно складывать только объекты типа Food")
+
+
+class Meal:
+    def __init__(self):
+        self.food_items = []
+
+    def add_food(self, food):
+        if isinstance(food, Food):
+            self.food_items.append(food)
+        else:
+            raise TypeError("Можно добавлять только объекты типа Food")
+
+    def __str__(self):
+        return "\n".join(str(food) for food in self.food_items)
+
+class BreakFast:
+    def __init__(self, list_of_meal):
+        self.__list_of_meal = list_of_meal
+
+    def get_list(self):
+        return self.__list_of_meal
+
+class Dinner:
+    def __init__(self, list_of_meal):
+        self.__list_of_meal = list_of_meal
+
+    def get_list(self):
+        return self.__list_of_meal
+
+class Lunch:
+    def __init__(self, list_of_meal):
+        self.__list_of_meal = list_of_meal
+
+    def get_list(self):
+        return self.__list_of_meal
+
+class Snack:
+    def __init__(self, list_of_meal):
+        self.__list_of_meal = list_of_meal
+
+    def get_list(self):
+        return self.__list_of_meal
 
 class ChildWindow(QDialog):
     def __init__(self, user):
         super().__init__()
         self.userC = user
-        self.init_ui(self.userC)
+        self.init_ui()
 
-    def init_ui(self, user):
-        self.setWindowTitle(f"CalCulc-user({user.get_name()})")
-        self.setGeometry(150, 150, 200, 150)
-
-        self.caloriesCount = user.calories_culc()
-
+    def init_ui(self):
+        self.setWindowTitle(f"CalCulc-user({self.userC.get_name()})")
+        self.setGeometry(150, 150, 500, 300)
         self.layout = QVBoxLayout()
 
-        self.calories_normal = QLabel('Ваша норма калорий: ' + str(self.caloriesCount))
+        caloriesCount = self.userC.calories_culc()
+        self.calories_normal = QLabel(f'Ваша норма калорий: {caloriesCount}')
+        self.layout.addWidget(self.calories_normal)
 
-        self.table = QTableWidget()
-        self.table.setRowCount(4)
-        self.table.setColumnCount(3)
-        self.table.setFixedSize(460, 250)
-        self.table.setColumnWidth(0, 100)
-        self.table.setColumnWidth(1, 120)
-        self.table.setColumnWidth(2, 100)
+        self.table = QTableWidget(4, 3)
         self.table.setHorizontalHeaderLabels(["Приём пищи", "Сколько съедено", "Цель"])
-
         data = [
-            ["Завтарк", "", str(self.caloriesCount / 4)],
-            ["Обед", "", str(self.caloriesCount / 4)],
-            ["Ужин", "", str(self.caloriesCount / 4)],
-            ["Перекус", "", str(self.caloriesCount / 4)],
+            ["Завтрак", "", str(caloriesCount / 4)],
+            ["Обед", "", str(caloriesCount / 4)],
+            ["Ужин", "", str(caloriesCount / 4)],
+            ["Перекус", "", str(caloriesCount / 4)]
         ]
 
-        for row in range(4):
-            for col in range(3):
-                item = QTableWidgetItem(data[row][col])
-                self.table.setItem(row, col, item)
+        for row, rowData in enumerate(data):
+            for col, value in enumerate(rowData):
+                self.table.setItem(row, col, QTableWidgetItem(value))
 
-        self.layout.addWidget(self.calories_normal)
         self.layout.addWidget(self.table)
         self.setLayout(self.layout)
+
 
 class MainWindow(QWidget):
     def __init__(self):
@@ -50,9 +154,18 @@ class MainWindow(QWidget):
         self.init_ui()
 
     def set_user_data(self):
-        self.user_data = User(self.enterName.text(), self.enterWeight.text(), self.enterHeight.text(), self.enterAge.text(), self.enterGender.text())
-        self.child_window = ChildWindow(self.user_data)  # Создаем экземпляр дочернего окна
-        self.child_window.exec()  # Показываем дочернее окно
+        try:
+            self.user_data = User(
+                self.enterName.text(),
+                self.enterWeight.text(),
+                self.enterHeight.text(),
+                self.enterAge.text(),
+                self.enterGender.text()
+            )
+            self.child_window = ChildWindow(self.user_data)
+            self.child_window.exec()
+        except ValueError:
+            self.user_label.setText("Ошибка: не корректные данные")
 
     def init_ui(self):
         self.setWindowTitle("CalCucl")
@@ -80,7 +193,6 @@ class MainWindow(QWidget):
         self.button.setFixedSize(100, 30)
         self.button.clicked.connect(self.set_user_data)
 
-
         self.layout.addWidget(self.user_label)
         self.layout.setAlignment(self.user_label, Qt.AlignmentFlag.AlignCenter)
 
@@ -105,78 +217,7 @@ class MainWindow(QWidget):
         self.setLayout(self.layout)
         self.show()
 
+
 app = QApplication(sys.argv)
 window = MainWindow()
-
-class Food:
-    def __init__(self, name_food, calories, squirrels, fats, carbohydrates):
-        self.__name_food = name_food
-        self.__calories = calories
-        self.__squirrels = squirrels
-        self.__fats = fats
-        self.__carbohydrates = carbohydrates
-
-class User:
-    def __init__(self, name_user, weight, height, years_old, gender):
-        self.__name_user = name_user
-        self.__weight = weight
-        self.__height = height
-        self.__years_old = years_old
-        self.__gender = gender
-
-    def show_data(self):
-        return [self.__name_user, self.__weight, self.__height, self.__years_old]
-
-    def get_name(self):
-        return self.__name_user
-
-    def get_weight(self):
-        return self.__weight
-
-    def get_height(self):
-        return self.__height
-
-    def get_age(self):
-        return self.__years_old
-
-    def get_gender(self):
-        return self.__gender
-
-    def calories_culc(self):
-        if self.__gender.lower() == 'male':
-            calories = (10 * int(self.__weight)) + (6.25 * int(self.__height)) - (5 * int(self.__years_old)) + 5
-            return calories
-        elif self.__gender.lower() == 'female':
-            calories = (10 * int(self.__weight)) + (6.25 * int(self.__height)) - (5 * int(self.__years_old)) - 161
-            return float(calories)
-
-class BreakFast:
-    def __init__(self, list_of_food):
-        self.__list_of_food = list_of_food
-
-    def get_list(self):
-        return self.__list_of_food
-
-class Dinner:
-    def __init__(self, list_of_food):
-        self.__list_of_food = list_of_food
-
-    def get_list(self):
-        return self.__list_of_food
-
-class Lunch:
-    def __init__(self, list_of_food):
-        self.__list_of_food = list_of_food
-
-    def get_list(self):
-        return self.__list_of_food
-
-class Snack:
-    def __init__(self, list_of_food):
-        self.__list_of_food = list_of_food
-
-    def get_list(self):
-        return self.__list_of_food
-
-
 sys.exit(app.exec())
